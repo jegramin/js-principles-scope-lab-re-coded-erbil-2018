89afb836926b03a4e12beb56d6cbcc5340c6b082
Mohammed Ameen <jegroamin@gmail.com>
2018-08-17 07:54:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,26 +16,3 @@ function overwriteBestCustomer(){
 function changeLeastFavoriteCustomer(){
   leastFavoriteCustomer = 'not me'
 }
-
-
-
-
-
-
-// customerName = 'bob';
-// function upperCaseCustomerName(){
-// customerName = 'BOB';
-// }
-
-// function setBestCustomer(){
-//   bestCustomer = 'not bob';
-// }
-
-// function overwriteBestCustomer(){
-//   bestCustomer = 'maybe bob';
-// }
-
-
-// function changeLeastFavoriteCustomer(){
-//     throw (TypeError);
-//   }
